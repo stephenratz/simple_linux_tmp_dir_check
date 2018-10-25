6896780c2e156629a51bb14e3d622c5f03bd7dd3
Stephen Ratz <sr185245@ncr.com>
2018-10-25 16:48:47 -0500

    Initial commit.

---
+++ README.md
@@ -0,0 +1,3 @@
+# Simple Linux Tmp Dir Check
+
+This is inteneded to be an example of a simple Linux Tmp directory existence check.
---
+++ controls/controls.rb
@@ -0,0 +1,15 @@
+# encoding: utf-8
+# copyright: 2018, Stephen Ratz
+# license: All rights reserved
+
+title 'simple windows users dir check'
+
+control 'test_1.1_check_tmp_directory_exists' do
+  impact 1.0
+  title 'Check Tmp directory exists'
+  desc 'This policy checks that the Tmp directory exists.'
+  describe directory( '/tmp' ) do
+    it { should exist }
+  end
+end
+
